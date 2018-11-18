8737171652718b56227dcb7ae422a75a9031c6e8
Denis José <Denis Jos@DenisJose>
2018-11-18 13:22:53 -0200

    Comitando a Alteração no gitHub

--- Empresa/src/Funcionário.java
+++ Empresa/src/Funcionário.java
@@ -8,5 +8,48 @@ public class Funcionário extends Cargo {
 	private String estado;
 	private double salario;
 	private Cargo codCargo;
+	
+	public int getCodFuncionario() {
+		return codFuncionario;
+	}
+	public void setCodFuncionario(int codFuncionario) {
+		this.codFuncionario = codFuncionario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public Cargo getCodCargo() {
+		return codCargo;
+	}
+	public void setCodCargo(Cargo codCargo) {
+		this.codCargo = codCargo;
+	}
 
 }
