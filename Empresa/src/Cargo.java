4ad9292ed17cb52739cd4994e880d927642bb78e
Denis José <Denis Jos@DenisJose>
2018-11-18 13:29:02 -0200

    Comitando a alteração da classe Cargo no gitHub

--- Empresa/src/Cargo.java
+++ Empresa/src/Cargo.java
@@ -3,4 +3,18 @@ public class Cargo {
 	
 	private int codCargo;
 	private String descricao;
+	
+	public int getCodCargo() {
+		return codCargo;
+	}
+	public void setCodCargo(int codCargo) {
+		this.codCargo = codCargo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
 }
